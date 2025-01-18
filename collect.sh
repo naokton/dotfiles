700d15a97ad9b9ef242453964aa8a53fb80ec695
naokton <march.of.rabbits@gmail.com>
2025-01-18 11:44:43 -0500

    Enhance collect command using rsync

--- collect.sh
+++ collect.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-CUR_DIR=$(dirname $0)
 
-copy_file(){
-    FILE=$1
-    DIR=$2
-    [ -n "$DIR" ] && mkdir -p $CUR_DIR/$DIR
-    cp $FILE $CUR_DIR/$DIR
-}
+set -euo pipefail
 
-# copy only file
-copy_file ~/.screenrc
-copy_file ~/.bashrc
-copy_file ~/.zshrc
-copy_file ~/.tmux.conf
-copy_file ~/.vimrc
-copy_file ~/.ripgreprc
-
-# mkdir and copy file into it
-copy_file ~/.emacs.d/init.el    .emacs.d
-copy_file ~/.config/pycodestyle .config
-
-exit 0
+WORK_DIR=$(dirname $0)
+cd $WORK_DIR
+rsync --files-from file_list.txt -arv ~/ .
