1bab506e9d34855f2ecafc432d86efdd49b5baa4
naokton <march.of.rabbits@gmail.com>
2025-10-05 18:42:29 -0400

    fix(emacs): trying orderless-flex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -123,6 +123,7 @@
   (completion-styles . '(orderless basic))
   (completion-category-defaults . nil)
   (completion-category-overrides . '((file (styles partial-completion))))
+  (orderless-matching-styles . '(orderless-literal orderless-regexp orderless-flex))
   )
 
 (leaf consult
