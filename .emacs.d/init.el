373d43c685e1f9ba5fc3e0b428c28d34c57546bd
naokton <march.of.rabbits@gmail.com>
2025-10-05 21:24:14 -0400

    fix(emacs): remove orderless-flex. Use "~" on demand instead.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -123,8 +123,7 @@
   (completion-styles . '(orderless basic))
   (completion-category-defaults . nil)
   (completion-category-overrides . '((file (styles partial-completion))))
-  (orderless-matching-styles . '(orderless-literal orderless-regexp orderless-flex))
-  )
+  (orderless-matching-styles . '(orderless-literal orderless-regexp)))
 
 (leaf consult
   :ensure t
