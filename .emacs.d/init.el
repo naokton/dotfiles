d9608238ebf17d6762c8750b0a788a7e568ba296
naokton <march.of.rabbits@gmail.com>
2026-09-11 09:58:22 -0400

    (emacs) Format and reorder

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -477,14 +477,13 @@ ref: URL `https://github.com/minad/consult/wiki#minads-orderless-configuration'"
   (ghostel-mode-hook . my/ghostel-override-projectile-cycling)
   (ghostel-exit-functions . my/ghostel-delete-windows-on-exit)
   :config
-  (defun my/ghostel-buffer-p (buf &optional _action)
-    (with-current-buffer buf (derived-mode-p 'ghostel-mode)))
   (defmacro my/ghostel-with-this-window (&rest body)
     "Run BODY, displaying the ghostel buffer it picks in the selected window."
     (declare (indent 0) (debug t))
     `(let ((display-buffer-overriding-action
             '(display-buffer-same-window (inhibit-same-window . nil))))
        ,@body))
+
   (defun my/ghostel-new-here ()
     (interactive)
     (my/ghostel-with-this-window
@@ -499,6 +498,7 @@ ref: URL `https://github.com/minad/consult/wiki#minads-orderless-configuration'"
     (interactive)
     (select-window (split-window-right))
     (my/ghostel-new-here))
+
   (defun my/ghostel-next-here ()
     "Show this project's next terminal in the selected window."
     (interactive)
@@ -515,10 +515,12 @@ ref: URL `https://github.com/minad/consult/wiki#minads-orderless-configuration'"
     :parent projectile-mode-map
     "C-." #'my/ghostel-next-here
     "C-," #'my/ghostel-previous-here)
+
   (defun my/ghostel-override-projectile-cycling ()
     "Cycle this project's terminals instead of its file buffers."
     (setf (alist-get 'projectile-mode minor-mode-overriding-map-alist)
           my/ghostel-projectile-override-map))
+
   (defun my/ghostel-side-window ()
     "Return the side window showing a ghostel buffer for the current project, if any."
     (seq-some (lambda (buf)
@@ -527,18 +529,23 @@ ref: URL `https://github.com/minad/consult/wiki#minads-orderless-configuration'"
               (if (project-current)
                   (ghostel-project-buffer-list)
                 (ghostel-buffer-list))))
+
   (defun my/ghostel-toggle ()
     "Toggle the project's ghostel side window, leaving normal windows alone."
     (interactive)
     (if-let* ((win (my/ghostel-side-window)))
         (delete-window win)
       (if (project-current) (ghostel-project) (ghostel))))
+
   (defun my/ghostel-delete-windows-on-exit (buf _event)
     "Close the windows showing BUF when its shell exits, except a frame's last one."
     (dolist (win (get-buffer-window-list buf nil t))
       ;; `window-parent' is nil for a frame's sole window
       (when (window-parent win)
         (delete-window win))))
+
+  (defun my/ghostel-buffer-p (buf &optional _action)
+    (with-current-buffer buf (derived-mode-p 'ghostel-mode)))
   (add-to-list 'display-buffer-alist
                '(my/ghostel-buffer-p
                 (display-buffer-in-side-window)
